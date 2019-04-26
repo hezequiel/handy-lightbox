3f737994b5d08f51eed957643e17974f0bfa4311
Hezequiel <hezequiel@anandaindia.org>
2019-04-26 13:15:01 +0200

    Add basic files

---
+++ handy-lightbox/handy-lightbox.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: Handy Lightbox
+ * Plugin URI: @TODO
+ * Description: Adds a lightbox functionality to your website
+ * Version: 0.5.0
+ * Author: Khushi Ananda
+ * Author URI: https://khushiananda.com
+ * License: GPL2
+ * Text Domain: handy-lightbox
+ * Domain Path: /languages
+ */
+
+define( 'KHA_LIGHTBOX_VERSION', '0.5.0' );
+define( 'KHA_LIGHTBOX_PATH', dirname(__FILE__) );
+define( 'KHA_LIGHTBOX_MODULES_PATH', KHA_LIGHTBOX_PATH . '/modules' );
+define( 'KHA_LIGHTBOX_URL', plugins_url( 'sticky-admin-menu', dirname(__FILE__) ) );
+
+
+require_once KHA_LIGHTBOX_PATH . '/functions/lightbox.php';
